a83db85ad446163969aa4b17c0c7a287ea71744b
Myha <miha@vidakovic.si>
2020-10-24 15:26:46 +0200

    added loader

--- src/pages/Admin/NotAvailable.js
+++ src/pages/Admin/NotAvailable.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Loader from 'react-loader-spinner'
 
 export default function NotAvailable() {
     return (
         <div className="subpage Manage">
-            Ta stran ne obstaja
+            <Loader
+                type="Rings"
+                color="#7BA47F"
+                height={60}
+                width={60}
+                timeout={3000} //3 secs
+                className="Loading"
+            />
         </div>
     )
 }
